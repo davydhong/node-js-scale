352373d10b94fe65e01dbac29f2f97acf6fe21c1
davydhong <davydhong@gmail.com>
2019-04-08 19:14:38 -0700

    simulate worker shutdown

---
+++ cluster/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log('this is the master process: ', process.pid);
+  for (let i = 0; i < numCPUs; i++) {
+    console.log(`forked #${i}`);
+    cluster.fork();
+  }
+  cluster.on('exit', worker => {
+    console.log(`worker process ${process.pid} has died`);
+    console.log(`only ${Object.keys(cluster.workers).length} remain`);
+  });
+} else {
+  http
+    .createServer((req, res) => {
+      const message = 'this is worker process: ' + process.pid;
+
+      res.end(`{process: ${process.pid}}`);
+      if (req.url === '/kill') {
+        process.exit();
+      } else {
+        console.log(`working on requrest: ${process.pid}...`);
+      }
+    })
+    .listen(3000);
+}
