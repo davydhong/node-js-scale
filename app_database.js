6e1d08a8993b5eb6e3038bc2726d00588ed0a9b1
davydhong <davydhong@gmail.com>
2019-04-08 21:11:31 -0700

    sharding a-m & n-z

--- app_database.js
+++ app_database.js
@@ -1,14 +1,16 @@
-const http = require('http')
-
-let requests = 0
+const http = require('http');
+const { LocalStorage } = require('node-localstorage');
+const db = new LocalStorage('./data');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        requests++
-        console.log(`${process.pid}: ${requests}`)
-        res.end(JSON.stringify(requests))
-    }
-})
+  if (req.url === '/') {
+    let requests = db.getItem('requests');
+    db.setItem('requests', ++requests);
 
-server.listen(3000)
-console.log(`counting requests`)
+    console.log(`${process.pid}: ${requests}`);
+    res.end(JSON.stringify(requests));
+  }
+});
+
+server.listen(3000);
+console.log(`counting requests`);
---
+++ shards/db.js
@@ -0,0 +1,32 @@
+const { LocalStorage } = require('node-localstorage');
+
+const dbA = new LocalStorage('data-m-z');
+const dbB = new LocalStorage('data-n-z');
+
+const whichDB = name => (name.match(/^[A-M]|^[a-m]/) ? dbA : dbB);
+
+const loadCats = db => JSON.parse(db.getItem('cats') || '[]');
+
+const hasCat = name =>
+  loadCats(whichDB(name))
+    .map(cat => cat.name)
+    .includes(name);
+
+module.exports = {
+  addCat(newCat) {
+    if (!hasCat(newCat.name)) {
+      let cats = loadCats(whichDB(newCat.name));
+      cats.push(newCat);
+      whichDB(newCat.name).setItem('cats', JSON.stringify(cats, null, 2));
+    }
+  },
+
+  findCatByName(name) {
+    let cats = loadCats(whichDB(name));
+    return cats.find(cat => cat.name === name);
+  },
+
+  findCatsByColor(color) {
+    return [...loadCats(dbA).filter(cat => cat.color === color), ...loadCats(dbB).filter(cat => cat.color === color)];
+  }
+};
